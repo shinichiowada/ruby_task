53bb6f506dfd6feb15fdfa508680e90d2cacc04b
shinichi <0503baku@gmail.com>
2020-03-02 22:37:09 +0900

    add_task

--- task.rb
+++ task.rb
@@ -43,16 +43,16 @@ end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
-
+  numbers2 = numbers1.map! {|i| i * 10 }
   # 以下に回答を記載
-  p [1,2,3,4,5].map {|n| n * 10}
+  p numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  p array.map!{|x| x.to_i}
+  p array.map(&:to_i)
 end
 
 def q8
@@ -94,6 +94,7 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  sports.flatten!.uniq!
   sports.each.with_index(1) do |sports, i|
   puts "No#{i} #{sports}"
   end
@@ -103,7 +104,7 @@ def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+  puts data[:user] [:name]
 end
 
 def q13
@@ -111,14 +112,14 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+p user_data.update(update_data)
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+p data.keys
 end
 
 def q15
@@ -126,7 +127,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  p data1.include?(:age) ? "OK" : "NG"
+  p data2.include?(:age) ? "OK" : "NG"
 end
 
 def q16
@@ -138,7 +140,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}です。"
+  end
 end
 
 class UserQ17
